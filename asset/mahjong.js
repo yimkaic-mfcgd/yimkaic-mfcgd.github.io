76f708f2c9af3344007efa251aab6a82a63c2ee1
Joseph Yim <74335727+yimkaic-mfcgd@users.noreply.github.com>
2026-06-14 02:13:16 +0100

    Add Excel epoch date handling in mahjong.js

--- asset/mahjong.js
+++ asset/mahjong.js
@@ -88,9 +88,10 @@ fetch(`https://docs.google.com/spreadsheets/d/e/2PACX-1vRwe0aPrTgSRP3cHuN1el-KYA
       const playerScores = values.slice(2, -2).map(v => Number(v || 0));
       const totalValue = Number(values[values.length - 2] || 0);
       const diffValue = Number(values[values.length - 1] || 0);
+      const excelEpoch = new Date(Date.UTC(1899, 11, 30)); // Excel base date
 
       return {
-        date: values[0],
+        date: /^\d{5}$/.test(values[0])? new Date((values[0] - 25569) * 86400000) : values[0],
         baseValue: values[1],
         playerScores,
         totalValue,
